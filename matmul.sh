5ce629af3ea60ba016303749f343a1a66f73bbf1
Karicho21 <kkato1@charlotte.edu>
2025-04-16 14:58:24 -0400

    final

--- matmul.sh
+++ matmul.sh
@@ -3,6 +3,7 @@
 #SBATCH --partition=GPU
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=8
 #SBATCH --gres=gpu:1
 #SBATCH --time=00:10:00
 module load cuda
@@ -11,9 +12,10 @@ nvcc -Xcompiler -fopenmp matmul.cu -lpthread -lcublas -o matmul
 
 output="output.txt"
 
+./matmul 256 4  >> $output
 ./matmul 512 4	>> $output
 ./matmul 1024 4	>> $output
-./matmul 2048 4 >> $output
+./matmul 2048 8 >> $output
 
 
 
